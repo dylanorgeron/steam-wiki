1ce8f143a3410de496131fb43c62f9184a460b4c
Dylan Orgeron <dylan.orgeron@gmail.com>
2016-04-08 13:57:39 -0500

    nav work

--- inc/head.php
+++ inc/head.php
@@ -1,5 +1,6 @@
 <title>STEAM Wiki</title>
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link href='https://fonts.googleapis.com/css?family=Nunito:400,300,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.2.min.js"></script>
 <script type="text/javascript" src="script.js"></script>
 
---
+++ inc/nav.php
@@ -0,0 +1,12 @@
+<nav>
+	<div class="container">
+		<a href="index.html">Home</a>
+		<a href="projects.html">Projects</a>
+		<a href="perspectives.html">Perspectives</a>
+		<a href="#">About</a>
+		<form class="pull-right">
+			<input type="text" />
+			<button>Search</button>
+		</form>
+	</div>
+</nav>
