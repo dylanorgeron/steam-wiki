85cc570ba83c3f932bb6b761dcc993a0f7da3b36
Dylan Orgeron <dylan.orgeron@gmail.com>
2016-04-08 13:32:29 -0500

    added php stuff

---
+++ inc/head.php
@@ -0,0 +1,5 @@
+<title>STEAM Wiki</title>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.2.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php ?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<?php include "inc/head.php"; ?>
+	</head>
+	<body>
+		<?php include "inc/nav.php"; ?>
+
+		
+
+
+		<?php include "inc/footer.php"; ?>
+	</body>
+</html>
+
